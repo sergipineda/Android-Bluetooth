a5609a9205d96f20e1e9b0af4c895d47a2993280
Sergi <spineda1994@gmail.com>
2016-02-16 19:20:33 +0100

    Mira si el bluetooth esta activat

--- app/src/main/java/com/iesebre/dam2/spineda/bluetooth/MainActivity.java
+++ app/src/main/java/com/iesebre/dam2/spineda/bluetooth/MainActivity.java
@@ -1,21 +1,60 @@
 package com.iesebre.dam2.spineda.bluetooth;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.bluetooth.BluetoothAdapter;
+import android.util.Log;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final int REQUEST_ENABLE_BT = 1;
+    private static final String TAG = "bluetooth MainActivity";
+    private BluetoothAdapter mBluetoothAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        BluetoothAdapter mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+        mBluetoothAdapter  = BluetoothAdapter.getDefaultAdapter();
+
         if (mBluetoothAdapter == null) {
             // Device does not support Bluetooth
             Toast.makeText(this, "Bluetooth is not available", Toast.LENGTH_LONG).show();
+        }else{
+            Toast.makeText(this, "Bluetooth is available", Toast.LENGTH_LONG).show();
         }
 
     }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        if (!mBluetoothAdapter.isEnabled()) {
+            Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+            startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT);
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        switch (requestCode) {
+            case REQUEST_ENABLE_BT:
+                if (resultCode == Activity.RESULT_OK) {
+                    // Bluetooth is now enabled, so set up a chat session
+                    Toast.makeText(this, "Bluetooth is not available", Toast.LENGTH_LONG).show();
+                } else {
+                    // User did not enable Bluetooth or an error occurred
+                    Log.d(TAG, "BT not enabled");
+                    Toast.makeText(this, "Bluetooth no s'ha activat correctament",
+                            Toast.LENGTH_SHORT).show();
+                    this.finish();
+                }
+        }
+    }
+
 }
+
