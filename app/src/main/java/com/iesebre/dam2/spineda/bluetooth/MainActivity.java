056a912733fa4abd29bb2f7588fd1ade8795fd6c
Sergi <spineda1994@gmail.com>
2016-02-16 19:07:56 +0100

    comprovar bluetooth

--- app/src/main/java/com/iesebre/dam2/spineda/bluetooth/MainActivity.java
+++ app/src/main/java/com/iesebre/dam2/spineda/bluetooth/MainActivity.java
@@ -2,6 +2,8 @@ package com.iesebre.dam2.spineda.bluetooth;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.bluetooth.BluetoothAdapter;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        BluetoothAdapter mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+        if (mBluetoothAdapter == null) {
+            // Device does not support Bluetooth
+            Toast.makeText(this, "Bluetooth is not available", Toast.LENGTH_LONG).show();
+        }
+
     }
 }
